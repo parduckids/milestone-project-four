5f27627329cf74387471c50c2d64b297e957a83a
Adam V <parduckids@gmail.com>
2024-06-11 22:54:48 +0000

    feat: add styling and start writing content for the home page

--- subsurface/settings.py
+++ subsurface/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
