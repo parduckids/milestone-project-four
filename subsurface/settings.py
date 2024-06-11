aeec721eb8636dcfea156a2a4ae29eb9ffe6dd18
Adam V <parduckids@gmail.com>
2024-06-11 22:30:07 +0000

    feat: add event app and home page

--- subsurface/settings.py
+++ subsurface/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,8 @@ INSTALLED_APPS = [
     # The following apps are required for the authentication:
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
+    # My apps
+    'event'
     
 ]
 
@@ -77,7 +79,10 @@ ROOT_URLCONF = 'subsurface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
