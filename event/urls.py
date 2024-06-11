7b1fc4512cdfc0600cd76c15857810cd72dba068
Adam V <parduckids@gmail.com>
2024-06-11 23:26:00 +0000

    feat: set up the look and feel of the events page

--- event/urls.py
+++ event/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('events', views.events, name='events')
 ]
--- event/views.py
+++ event/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index (request):
     """ A view to return the index page """
     return render(request, 'event/index.html')
+
+def events (request):
+    """ A view to return the events page """
+    return render(request, 'event/events.html')
