913263aed72ca4aa325dba57bf1eaf6c4a0e1b83
Adam V <parduckids@gmail.com>
2024-06-12 22:22:23 +0000

    feat: add event detail page that uses real data

--- event/urls.py
+++ event/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('events', views.events, name='events'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
     path('create-event', views.create_event, name='create')
 ]
--- event/views.py
+++ event/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EventForm 
 from .models import Event
 
-# Create your views here.
 
 def index (request):
     """ A view to return the index page """
@@ -14,13 +13,28 @@ def events (request):
     return render(request, 'event/events.html', {'events': events})
 
 def create_event(request):
+    """ A view to return the create event page """
+    # todo: only allow this to the admin user
     if request.method == "POST":
         form = EventForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('events')
         else:
-            print(form.errors)  # Print form errors if form is not valid
+            print(form.errors)  
     else:
         form = EventForm()
     return render(request, 'event/create_event.html', {'form': form})
+
+
+def event_detail(request, event_id):
+    """ A view to return the event detail pages """
+    event = get_object_or_404(Event, event_id=event_id)
+    artist_list = event.artists.split(',')
+    context = {
+        'event': event,
+        'artist_list': artist_list,
+    }
+    return render(request, 'event/event_detail.html', context)
+
+    
