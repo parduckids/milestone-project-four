c9cce7741b55de58ef69f8d763e2036c051ab948
Adam V <parduckids@gmail.com>
2024-06-12 23:51:56 +0000

    feat: add basic manage events options with edit and delete functionality

--- event/urls.py
+++ event/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('events', views.events, name='events'),
     path('event/<int:event_id>/', views.event_detail, name='event_detail'),
-    path('create-event', views.create_event, name='create')
+    path('create-event', views.create_event, name='create'),
+    path('manage-events', views.manage_events, name='manage'),
+    path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
+    path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
 ]
--- event/views.py
+++ event/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EventForm 
 from .models import Event
 
-
+# todo: add actual events, show only free, the newest date ones
 def index (request):
     """ A view to return the index page """
     return render(request, 'event/index.html')
-
+# todo: filtering events 
 def events (request):
     """ A view to return the events page """
     events = Event.objects.all()
@@ -26,7 +26,35 @@ def create_event(request):
         form = EventForm()
     return render(request, 'event/create_event.html', {'form': form})
 
+# todo: filtering events 
+def manage_events(request):
+    """ A view to return the manage events page """
+    events = Event.objects.all()
+    return render(request, 'event/manage.html', {'events': events})
 
+# todo: event data doesn't show on edit event page when normal layout is set, layout needs to be changed, image should be shown
+def edit_event(request, event_id):
+    """ A view to return the edit event page """
+    event = get_object_or_404(Event, event_id=event_id)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('manage')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'event/edit_event.html', {'form': form})
+
+# todo: event data doesn't show on delete event page
+def delete_event(request, event_id):
+    """ A view for deleting an event """
+    event = get_object_or_404(Event, event_id=event_id)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('manage')
+    return render(request, 'event/delete_event.html', {'event': event})
+
+# todo: set up tickets app
 def event_detail(request, event_id):
     """ A view to return the event detail pages """
     event = get_object_or_404(Event, event_id=event_id)
