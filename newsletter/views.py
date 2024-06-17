372c6140bfb22dd832e4a49ec3d6c6577fe43920
Adam V <parduckids@gmail.com>
2024-06-17 22:38:29 +0000

    feat: add confirmation email functionality to the newsletter app

--- newsletter/views.py
+++ newsletter/views.py
@@ -8,6 +8,17 @@ def subscribe(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Thank you for subscribing!')
+             # send confirmation email
+            subject = 'Subsurface | Subscription Confirmation'
+            message = 'Thank you for subscribing to our newsletter! Have a great day!'
+            recipient_email = form.cleaned_data['email']
+            email_from = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [recipient_email]
+
+            try:
+                send_mail(subject, message, email_from, recipient_list, fail_silently=False)
+            except Exception as e:
+                messages.error(request, 'There was an error sending the confirmation email: {}'.format(e))
             # set session variable to check subscription
             request.session['subscribed'] = True
             return redirect(request.META.get('HTTP_REFERER', '/'))
