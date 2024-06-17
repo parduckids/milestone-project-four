4591ef2d3cef88b2f1100e08c3eeae05b589f5eb
Adam V <parduckids@gmail.com>
2024-06-17 22:40:07 +0000

    fix: add missing imports to the newsletter app views

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import SubscribeForm
 
 def subscribe(request):
