2b1d6129bdb658cda6b7511aeb6a063daf4a28ae
Adam V <parduckids@gmail.com>
2024-06-17 02:38:10 +0000

    feat: set up live emailing

--- subsurface/settings.py
+++ subsurface/settings.py
@@ -65,14 +65,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # send emails to console 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # todo: live version --> send emails for real
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = 'ads.voros@gmail.com'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
