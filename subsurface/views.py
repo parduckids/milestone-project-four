3b0ac4dcfd958611c29225c259cbf99327ad0837
Adam V <parduckids@gmail.com>
2024-06-18 01:12:51 +0000

    fix: try to revert

--- subsurface/views.py
+++ subsurface/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
-# # custom 500 error handler
-# def custom_500(request):
-#     return render(request, '500.html', status=500)
+# custom 500 error handler
+def custom_500(request):
+    return render(request, '500.html', status=500)
 
 # custom 403 error handler
 def custom_403(request, exception):
